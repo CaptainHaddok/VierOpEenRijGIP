8140448aba040e6f2e4b33c4a7f566395e82e0b6
CaptainHaddok <warre.bruyninckx@hotmail.com>
2019-12-05 19:56:50 +0100

    alles wat we al hadden gëimporteerd in Gip repositry

---
+++ Main.py
@@ -0,0 +1,19 @@
+
+
+import Bord
+player1 = 1
+player2 = 2
+example = Bord.Bord("Bord 1",player1,player2)
+
+while True:
+    inpt1 = int(input("Welke rij?"))
+
+    example.PutIn(inpt1,player1)
+    print(example)
+
+    inpt1 = int(input("Welke rij?"))
+
+    example.PutIn(inpt1, player2)
+
+
+    print(example)
---
+++ ScoreChekker.py
@@ -0,0 +1,23 @@
+import Bord as B
+def Check2InRow(bord, player):
+    score = 0
+    # TODO: cheker fixen
+    return score
+
+
+def Check3InRow(bord, player):
+    score = 0
+    # TODO: cheker fixen
+    return score
+
+
+def CheckMiddle(bord, player):
+    score = 0
+    # TODO: cheker fixen
+    return score
+
+
+def CheckWin(bord, player):
+    score = 0
+    # TODO: cheker fixen
+    return score
